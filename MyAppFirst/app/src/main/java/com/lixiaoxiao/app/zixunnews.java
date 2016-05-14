4d6930a71e2306f37baaecb813e1508adee4d57e
1119093254 <1119093254@qq.com>
2016-05-14 11:06:20 +0800

    建立自己的工作包

---
+++ MyAppFirst/app/src/main/java/com/lixiaoxiao/app/zixunnews.java
@@ -0,0 +1,13 @@
+package com.lixiaoxiao.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class zixunnews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zixunnews);
+    }
+}
